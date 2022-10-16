efd399a1fd4351531e3dbf84590d98911bdaefcd
Andrea Lou <andrealou@Andreas-MacBook-Pro.local>
2022-10-16 00:28:51 -0700

    style stuff

--- components/Post.jsx
+++ components/Post.jsx
@@ -4,6 +4,8 @@ import { Text, View, Dimensions, StyleSheet, TouchableOpacity, Alert } from "rea
 import Constants from 'expo-constants';
 import DialogInput from "react-native-dialog-input";
 
+import { Card } from 'react-native-paper';
+
 const DEVICE_WIDTH = Dimensions.get("window").width;
 const DEVICE_HEIGHT = Dimensions.get("window").height;
 
@@ -16,7 +18,7 @@ export default function Post() {
   const [inputNumber, setInputNumber] = useState(0);
   //Name, Age, Gender, School, Bio
   const [commentData, setCommentData] = useState([
-    "Not set",
+    "",
   ]);
 
   function callDialog(attribute) {
@@ -28,7 +30,7 @@ export default function Post() {
   }
   
   return (
-    <View style = {styles.container}>
+    <View>
     <View
       style={styles.border}
     >
@@ -60,7 +62,7 @@ export default function Post() {
       {/**Dates*/}
       <View style={{ flexDirection: "row" }}>
         <Text style={styles.standard}>Time</Text>
-        <Text style={styles.standard}> - to - </Text>
+        <Text style={styles.standard}>- to -</Text>
         <Text style={styles.standard}>Time</Text>
       </View>
       {/**Details */}
@@ -69,12 +71,14 @@ export default function Post() {
           Come with me to wonderful Davis California!
         </Text>
       </View>
-        <Text style={styles.standard}>{commentData[0]}</Text>
-        <TouchableOpacity onPress={() => callDialog("Connect Me!")}>
-            <Text>Comment</Text>
-        </TouchableOpacity>
-      <View>
 
+      <View style = {styles.container}>
+        <Card style={styles.comment}>
+          <Text style={styles.standard}>{commentData[0]}</Text>
+          <TouchableOpacity onPress={() => callDialog("Connect Me!")}>
+            <Text style= {styles.standard}>Comment</Text>
+          </TouchableOpacity>
+        </Card>
       </View>
 
       {/**More*/}
@@ -86,13 +90,25 @@ export default function Post() {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
     justifyContent: 'center',
-    paddingTop: Constants.statusBarHeight,
-    backgroundColor: '#ecf0f1',
-    padding: 8,
+    width: DEVICE_WIDTH / 1.2,
+
+    flex: 1,
+
+    borderRadius: 10,
+    backgroundColor: '#ecf0f1', // light pink purple
+
+    borderWidth: 3,
+    borderColor: '#7c5295',
   },
-  
+
+  comment: {
+    backgroundColor: '#dfc0eb',
+    flex: 1,
+    padding: 5,
+  },
+
+
   border: {
     width: DEVICE_WIDTH / 1.1,
     height: DEVICE_WIDTH / 1.5,
@@ -100,27 +116,23 @@ const styles = StyleSheet.create({
     justifyContent: "top",
     alignItems: "left",
     borderRadius: 10,
-    backgroundColor: "white",
-    borderWidth: 10,
-    borderColor: "purple",
+    backgroundColor: '#bca0dc', //light purple
+    borderWidth: 3,
+    borderColor: '#7c5295', //mid dark purple
     padding: 10,
     
   },
 
   standard: {
-    fontSize: 20,
+    fontSize: 15,
+    padding: 5,
+    fontFamily: "Cochin",
   },
 
   big: {
     justifyContent: "center",
     fontSize: 30,
+    
   },
 
-
-  paragraph: {
-    margin: 24,
-    fontSize: 18,
-    fontWeight: 'bold',
-    textAlign: 'center',
-  },
 });
