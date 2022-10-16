766e596b6657a98ec4ab4adb8218a0a92043d75a
aidansunbury <aidansunbury@gmail.com>
2022-10-16 01:22:25 -0700

    headerstyles baby

--- components/Post.jsx
+++ components/Post.jsx
@@ -1,25 +1,27 @@
 import React, { useState } from "react";
 
-import { Text, View, Dimensions, StyleSheet, TouchableOpacity, Alert } from "react-native";
-import Constants from 'expo-constants';
+import {
+  Text,
+  View,
+  Dimensions,
+  StyleSheet,
+  TouchableOpacity,
+  Alert,
+} from "react-native";
+import Constants from "expo-constants";
 import DialogInput from "react-native-dialog-input";
 
-import { Card } from 'react-native-paper';
+import { Card } from "react-native-paper";
 
 const DEVICE_WIDTH = Dimensions.get("window").width;
 const DEVICE_HEIGHT = Dimensions.get("window").height;
 
-
-
 export default function Post() {
-  
   const [dialogVisible, setDialogVisible] = useState(false); // Visible Dialog Box
   const [hint, setHint] = useState("");
   const [inputNumber, setInputNumber] = useState(0);
   //Name, Age, Gender, School, Bio
-  const [commentData, setCommentData] = useState([
-    "",
-  ]);
+  const [commentData, setCommentData] = useState([""]);
 
   function callDialog(attribute) {
     setHint(attribute);
@@ -28,99 +30,93 @@ export default function Post() {
     }
     setDialogVisible(true);
   }
-  
+
   return (
     <View>
-    <View
-      style={styles.border}
-    >
-      
-      <DialogInput
-        isDialogVisible={dialogVisible}
-        hintInput={hint}
-        submitInput={(inputText) => {
-          setCommentData((commentData) => ({
-            ...commentData,
-            [inputNumber]: inputText,
-          }));
-          setDialogVisible(!dialogVisible);
-        }}
-        closeDialog={() => {
-          setDialogVisible(!dialogVisible);
-        }}
-      ></DialogInput>
-    
+      <View style={styles.border}>
+        <DialogInput
+          isDialogVisible={dialogVisible}
+          hintInput={hint}
+          submitInput={(inputText) => {
+            setCommentData((commentData) => ({
+              ...commentData,
+              [inputNumber]: inputText,
+            }));
+            setDialogVisible(!dialogVisible);
+          }}
+          closeDialog={() => {
+            setDialogVisible(!dialogVisible);
+          }}
+        ></DialogInput>
 
-      {/**Location */}
-      <View style={{ borderBottomWidth: 2 }}>
-        <View style={{ flexDirection: "row" }}>
-          <Text style={styles.big}>Start</Text>
-          <Text style={styles.big}> - to - </Text>
-          <Text style={styles.big}>End</Text>
+        {/**Location */}
+        <View style={{ borderBottomWidth: 2 }}>
+          <View style={{ flexDirection: "row" }}>
+            <Text style={styles.big}>Start</Text>
+            <Text style={styles.big}> - to - </Text>
+            <Text style={styles.big}>End</Text>
+          </View>
         </View>
-      </View>
-      {/**Dates*/}
-      <View style={{ flexDirection: "row" }}>
-        <Text style={styles.standard}>Time</Text>
-        <Text style={styles.standard}>- to -</Text>
-        <Text style={styles.standard}>Time</Text>
-      </View>
-      {/**Details */}
-      <View>
-        <Text style={styles.standard}>
-          Come with me to wonderful Davis California!
-        </Text>
-      </View>
+        {/**Dates*/}
+        <View style={{ flexDirection: "row" }}>
+          <Text style={styles.standard}>Time</Text>
+          <Text style={styles.standard}>- to -</Text>
+          <Text style={styles.standard}>Time</Text>
+        </View>
+        {/**Details */}
+        <View>
+          <Text style={styles.standard}>
+            Come with me to wonderful Davis California!
+          </Text>
+        </View>
 
-      <View style = {styles.container}>
-        <Card style={styles.comment}>
-          <Text style={styles.standard}>{commentData[0]}</Text>
-          <TouchableOpacity onPress={() => callDialog("Connect Me!")}>
-            <Text style= {styles.standard}>Comment</Text>
-          </TouchableOpacity>
-        </Card>
-      </View>
+        <View style={styles.container}>
+          <Card style={styles.comment}>
+            <Text style={styles.standard}>{commentData[0]}</Text>
+            <TouchableOpacity onPress={() => callDialog("Connect Me!")}>
+              <Text style={styles.standard}>Comment</Text>
+            </TouchableOpacity>
+          </Card>
+        </View>
 
-      {/**More*/}
-      <View></View>
-    </View>
+        {/**More*/}
+        <View></View>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    justifyContent: 'center',
+    justifyContent: "center",
     width: DEVICE_WIDTH / 1.2,
 
     flex: 1,
 
     borderRadius: 10,
-    backgroundColor: '#ecf0f1', // light pink purple
+    backgroundColor: "#ecf0f1", // light pink purple
 
     borderWidth: 3,
-    borderColor: '#7c5295',
+    borderColor: "#7c5295",
   },
 
   comment: {
-    backgroundColor: '#dfc0eb',
+    backgroundColor: "#dfc0eb",
     flex: 1,
     padding: 5,
   },
 
-
   border: {
     width: DEVICE_WIDTH / 1.1,
     height: DEVICE_WIDTH / 1.5,
-    
+
     justifyContent: "top",
     alignItems: "left",
     borderRadius: 10,
-    backgroundColor: '#bca0dc', //light purple
+    backgroundColor: "#bca0dc", //light purple
     borderWidth: 3,
-    borderColor: '#7c5295', //mid dark purple
+    borderColor: "#7c5295", //mid dark purple
     padding: 10,
-    
   },
 
   standard: {
@@ -132,7 +128,5 @@ const styles = StyleSheet.create({
   big: {
     justifyContent: "center",
     fontSize: 30,
-    
   },
-
 });
